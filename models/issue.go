2dccacc10464679222ba06f22227e42c2574de06
CatBloom <shomail.cat@gmail.com>
2023-07-10 19:22:26 +0900

    イシューを登録

--- models/issue.go
+++ models/issue.go
@@ -13,6 +13,7 @@ import (
 type IssueModel interface {
 	GetIssues(string, types.IssuesReq) (types.IssuesRes, error)
 	GetIssue(string, types.IssueReq) (types.IssueRes, error)
+	CreateIssue(string, types.IssueCreateReq) (types.IssueCreateRes, error)
 }
 
 type issueModel struct{}
@@ -168,6 +169,67 @@ func (im *issueModel) GetIssue(token string, issueReq types.IssueReq) (types.Iss
 	return issueRes, nil
 }
 
+func (im *issueModel) CreateIssue(token string, issueCreateReq types.IssueCreateReq) (types.IssueCreateRes, error) {
+	issueCreateRes := types.IssueCreateRes{}
+
+	query := `
+		mutation createIssue($input: CreateIssueInput!) {
+			createIssue(input: $input) {
+		  		issue {
+					id
+					createdAt
+					updatedAt
+					state
+					url
+					title
+					number
+					body
+					bodyHTML
+		  		}
+			}
+	  	}
+	`
+
+	val := map[string]string{"query": query, "variables": im.makeVariables(issueCreateReq)}
+
+	data, err := json.Marshal(val)
+	if err != nil {
+		log.Println("Error marshal GraphQL query json:", err)
+		return issueCreateRes, err
+	}
+
+	req, err := http.NewRequest(http.MethodPost, configs.GetGithubGraphQLEndPoint(), strings.NewReader(string(data)))
+	if err != nil {
+		log.Println("Error request github api:", err)
+		return issueCreateRes, err
+	}
+
+	req.Header.Set("Authorization", token)
+	req.Header.Set("Context-type", "application/json")
+
+	client := &http.Client{
+		CheckRedirect: func(req *http.Request, via []*http.Request) error {
+			return http.ErrUseLastResponse
+		},
+	}
+	res, err := client.Do(req)
+	if err != nil {
+		log.Println("Error do client:", err)
+		return issueCreateRes, err
+	}
+
+	defer res.Body.Close()
+	body, err := io.ReadAll(res.Body)
+	if err != nil {
+		log.Println("Error reading body:", err)
+		return issueCreateRes, err
+	}
+
+	json.Unmarshal(body, &issueCreateRes)
+
+	return issueCreateRes, nil
+}
+
 func (rm *issueModel) makeVariables(i interface{}) string {
 	variables := make(map[string]interface{})
 
@@ -203,6 +265,18 @@ func (rm *issueModel) makeVariables(i interface{}) string {
 		variables["name"] = obj.Repo
 		variables["number"] = obj.Number
 		variables["last"] = 100 // コメントは最新の100件を取得
+	}
+
+	// IssueCreateReq型の場合
+	if obj, ok := i.(types.IssueCreateReq); ok {
+		input := make(map[string]interface{})
+		input["repositoryId"] = obj.RepoID
+		input["title"] = obj.Title
+		input["body"] = obj.Body
+		if len(obj.LabelIds) != 0 {
+			input["labelIds"] = obj.LabelIds
+		}
+		variables["input"] = input
 	}
 
 	jsonVariables, err := json.Marshal(variables)
